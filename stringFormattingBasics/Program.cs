98b9a9093de825bf6b79aacc871d3bbddc957a85
Mark Hernandez <markkimhernandez@gmail.com>
2024-02-18 17:28:50 -0600

    String formatting basics

---
+++ stringFormattingBasics/Program.cs
@@ -0,0 +1,10 @@
+﻿string first = "Hello";
+string second = "World";
+// string result = string.Format("{0}, {1}!", first, second);
+// Console.WriteLine(result);
+
+// string result = string.Format("{1}, {0}!", first, second);
+// Console.WriteLine(result);
+
+string result = string.Format("{1}, {1}, {1}", first, second);
+Console.WriteLine(result);
